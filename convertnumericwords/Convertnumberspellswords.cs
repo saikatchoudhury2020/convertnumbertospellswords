4a7a337c6f5422c20dc164931b70f90fb38d0e83
saikatchoudhury2020 <saikathchowdhury7@gmail.com>
2020-05-08 23:50:42 +0530

    Convertnumberspellswords
    create class

--- convertnumericwords/Convertnumberspellswords.cs
+++ convertnumericwords/Convertnumberspellswords.cs
@@ -7,6 +7,44 @@ namespace convertnumbertospellswords
 {
     public class Convertnumberspellswords
     {
+        private static String ones(String Number)
+        {
+            int _Number = Convert.ToInt32(Number);
+            String name = "";
+            switch (_Number)
+            {
+
+                case 1:
+                    name = "One";
+                    break;
+                case 2:
+                    name = "Two";
+                    break;
+                case 3:
+                    name = "Three";
+                    break;
+                case 4:
+                    name = "Four";
+                    break;
+                case 5:
+                    name = "Five";
+                    break;
+                case 6:
+                    name = "Six";
+                    break;
+                case 7:
+                    name = "Seven";
+                    break;
+                case 8:
+                    name = "Eight";
+                    break;
+                case 9:
+                    name = "Nine";
+                    break;
+            }
+            return name;
+        }
+
 
     }
 }
