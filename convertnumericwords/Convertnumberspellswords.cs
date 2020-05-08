34a34fc78682953c5f5b5ace05c8ee21e4c6c0ad
saikatchoudhury2020 <saikathchowdhury7@gmail.com>
2020-05-08 23:51:54 +0530

    Update Convertnumberspellswords.cs
    create tens methods

--- convertnumericwords/Convertnumberspellswords.cs
+++ convertnumericwords/Convertnumberspellswords.cs
@@ -45,6 +45,75 @@ namespace convertnumbertospellswords
             return name;
         }
 
+        private static String tens(String Number)
+        {
+            int _Number = Convert.ToInt32(Number);
+            String name = null;
+            switch (_Number)
+            {
+                case 10:
+                    name = "Ten";
+                    break;
+                case 11:
+                    name = "Eleven";
+                    break;
+                case 12:
+                    name = "Twelve";
+                    break;
+                case 13:
+                    name = "Thirteen";
+                    break;
+                case 14:
+                    name = "Fourteen";
+                    break;
+                case 15:
+                    name = "Fifteen";
+                    break;
+                case 16:
+                    name = "Sixteen";
+                    break;
+                case 17:
+                    name = "Seventeen";
+                    break;
+                case 18:
+                    name = "Eighteen";
+                    break;
+                case 19:
+                    name = "Nineteen";
+                    break;
+                case 20:
+                    name = "Twenty";
+                    break;
+                case 30:
+                    name = "Thirty";
+                    break;
+                case 40:
+                    name = "Fourty";
+                    break;
+                case 50:
+                    name = "Fifty";
+                    break;
+                case 60:
+                    name = "Sixty";
+                    break;
+                case 70:
+                    name = "Seventy";
+                    break;
+                case 80:
+                    name = "Eighty";
+                    break;
+                case 90:
+                    name = "Ninety";
+                    break;
+                default:
+                    if (_Number > 0)
+                    {
+                        name = tens(Number.Substring(0, 1) + "0") + " " + ones(Number.Substring(1));
+                    }
+                    break;
+            }
+            return name;
+        }
 
     }
 }
