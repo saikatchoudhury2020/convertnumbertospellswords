4cd75631b9cd5e62ee46181c574f1b7fa8207168
saikatchoudhury2020 <saikathchowdhury7@gmail.com>
2020-05-08 23:57:45 +0530

    create method ConvertDecimals
    create method ConvertDecimals

--- convertnumericwords/Convertnumberspellswords.cs
+++ convertnumericwords/Convertnumberspellswords.cs
@@ -219,6 +219,23 @@ namespace convertnumbertospellswords
             catch { }
             return val;
         }
-
+        private static String ConvertDecimals(String number)
+        {
+            String cd = "", digit = "", engOne = "";
+            for (int i = 0; i < number.Length; i++)
+            {
+                digit = number[i].ToString();
+                if (digit.Equals("0"))
+                {
+                    engOne = "Zero";
+                }
+                else
+                {
+                    engOne = ones(digit);
+                }
+                cd += " " + engOne;
+            }
+            return cd;
+        }
     }
 }
