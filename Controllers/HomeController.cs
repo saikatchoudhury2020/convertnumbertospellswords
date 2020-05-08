2e4145855ae59529626aacc7794484a6f23505f6
saikatchoudhury2020 <saikathchowdhury7@gmail.com>
2020-05-08 23:42:18 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace convertnumbertospellswords.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string txtDigits)
+        {
+            string isNegative = "";
+            try
+            {
+                string number = Convert.ToDouble(txtDigits).ToString();
+
+                if (number.Contains("-"))
+                {
+                    isNegative = "Minus ";
+                    number = number.Substring(1, number.Length - 1);
+                }
+                if (number == "0")
+                {
+                    ViewBag.Name ="The number in currency fomat is Zero Only";
+                }
+                else
+                {
+                    ViewBag.Name = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
